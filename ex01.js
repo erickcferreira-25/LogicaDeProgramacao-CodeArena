23c091acd0cdbdf5292223571c3204e0c9a905d7
erickcferreira-25 <erickcferreira25@gmail.com>
2026-04-14 08:33:06 -0300

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -8,12 +8,3 @@ console.log("Olá, Alice! Bem-vinda de volta!")
 } else {
 console.log(`Olá, ${nome}! Prazer em conhecê-lo!`)
  }
-
-//Atividade 2
-const readline = require('readline-sync');
-
-let nome = readline.question("Digite seu nome: ")
-let curso = readline.question("Qual seu curso: ")
-let idade = readline.question("Qual sua idade: ")
-
-console.log(`Olá, ${nome}! Você tem ${idade} anos e está cursando ${curso}, Bem-vindo!`)

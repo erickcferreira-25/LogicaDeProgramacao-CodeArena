ceb742edada2c8c1621c13860f570e21db39d576
erickcferreira-25 <erickcferreira25@gmail.com>
2026-04-14 09:38:40 -0300

    Update ex07.js

--- ex07.js
+++ ex07.js
@@ -1,14 +1,14 @@
 
 const readline = require("readline-sync")
 
-let mediaAprovacao = 7
+let mediaAprovacao = 5
 
 let notaFinal = readline.questionFloat("Informe a nota final do aluno: ")
 
-console.log("\n========== Resultado Final ==========\n")
+console.log("========= Resultado Final =========")
 if (notaFinal >= mediaAprovacao) {
     console.log(`Aprovado! O aluno atingiu a nota mínima de ${mediaAprovacao}.`)
 } else {
     console.log(`Reprovado! O aluno não atingiu a nota mínima de ${mediaAprovacao}.`)
 }
-console.log("\n====================================\n")
+console.log("==================================")

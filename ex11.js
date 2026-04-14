adaa3ccaea8a08af277827da3442ef792cea1e46
erickcferreira-25 <erickcferreira25@gmail.com>
2026-04-14 09:41:21 -0300

    Update ex11.js

--- ex11.js
+++ ex11.js
@@ -6,6 +6,6 @@ let tamanhoTotal = 20
 let quantidadeBaixada = prompt.questionInt('Quantos gigas foram baixados?: ')
 let percentual = (quantidadeBaixada / tamanhoTotal) * 100
 
-console.log('\n========== Progresso do Download ==========' )
+console.log('========== Progresso do Download ==========' )
 console.log(`Você já baixou ${Math.round(percentual)}% do arquivo.`)
-console.log('========================================\n')
+console.log('========================================')
